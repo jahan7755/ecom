fb2aaf9e725a9fc18919a20b884ff34d2507539c
jahan khokhar <jahankhokhar@gmail.com>
2024-06-19 11:58:24 +0530

    create logic

--- resources/views/Brands/Create.blade.php
+++ resources/views/Brands/Create.blade.php
@@ -9,49 +9,56 @@
       </div>
       <div class="card-body">
 
-        <form action="" method="POST">
+        <form action="" enctype="multipart/form-data" method="POST">
 
             @csrf
 
           <div class="row mb-3">
             <label class="col-sm-2 col-form-label" for="name">Name</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="name" name="name" />
+              <input type="text" class="form-control" id="name" name="name"   />
             </div>
           </div>
 
           <div class="row mb-3">
             <label class="col-sm-2 col-form-label" for="seller">Seller</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="seller" name="seller" />
+              <input type="text" class="form-control" id="seller" name="seller"  />
             </div>
           </div>
 
           <div class="row mb-3">
             <label class="col-sm-2 col-form-label" for="origin">Origin</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="origin" name="origin" />
+              <input type="text" class="form-control" id="origin" name="origin"  />
             </div>
           </div>
 
           <div class="row mb-3">
             <label class="col-sm-2 col-form-label" for="location">Location</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="location" name="location" />
+              <input type="text" class="form-control" id="location" name="location"  />
             </div>
           </div>
 
           <div class="row mb-3">
             <label class="col-sm-2 col-form-label" for="rating">Rating</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="rating" name="rating" />
+              <input type="text" class="form-control" id="rating" name="rating"  />
             </div>
           </div>
+
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="logo">Logo</label>
+            <div class="col-sm-10">
+                <input type="file" class="form-control-file" id="logo" name="logo" accept="image/*" />
+            </div>
+        </div>
           
          
           <div class="row justify-content-end">
             <div class="col-sm-10">
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" class="btn btn-primary">Save</button>
             </div>
           </div>
         </form>
